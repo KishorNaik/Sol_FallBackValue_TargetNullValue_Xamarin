42135c7d3305f2bb85ecd2fbeb080f716b1deaab
Kishor Naik <kishor.naik011.net@outlook.com>
2020-12-11 20:49:06 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/Models/CustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sol_Demo.Models
+{
+    public class CustomerModel : ObservableObject
+    {
+        private String fullName;
+
+        public String FullName
+        {
+            get => fullName;
+            set => base.SetProperty(ref fullName, value);
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Sol_Demo/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MvvmHelpers;
+using Sol_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sol_Demo.ViewModels
+{
+    public class CustomerViewModel : BaseViewModel
+    {
+        public CustomerViewModel()
+        {
+            Customer = new CustomerModel();
+            Customer.FullName = null;
+        }
+
+        private CustomerModel customerModel = null;
+
+        public CustomerModel Customer
+        {
+            get => this.customerModel;
+            set => base.SetProperty(ref customerModel, value);
+        }
+    }
+}
